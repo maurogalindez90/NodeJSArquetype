c92578393618049cfc677fbc9adc66b27003edca
maurogalindez90 <maurogalindez90@github.com>
2022-03-09 12:39:24 -0300

    merge

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,11 +1,19 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+<<<<<<< HEAD
+=======
+import { Observable } from "rxjs";
+>>>>>>> 279bd74... commit para subir a gitlab
 
 @Injectable({ providedIn: 'root' })
 export class HttpService {
 
     constructor(private httpClient: HttpClient) {}
 
+<<<<<<< HEAD
     public getBands = ()  => this.httpClient.get('http://localhost:3000/bands');
+=======
+    public getBands = () : Observable<any> => this.httpClient.get('http://localhost:3000/bands');
+>>>>>>> 279bd74... commit para subir a gitlab
     
 }
