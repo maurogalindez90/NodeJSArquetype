dc65c5c9aaa72f1227e358be1a76359a1b375f92
maurogalindez90 <maurogalindez90@github.com>
2022-03-11 18:03:46 -0300

    se instala cors

--- src/cmd/middleware.ts
+++ src/cmd/middleware.ts
@@ -1,7 +1,9 @@
 import express = require('express');
 import bodyParser = require('body-parser');
+let cors = require('cors');
 
 export default (app: express.Application) => {
     app.use(bodyParser.json({limit: "50mb"}));
+    app.use(cors());
     // sumar todo middleware que se quiera utilizar
 }
